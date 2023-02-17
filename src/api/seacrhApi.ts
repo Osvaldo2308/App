994e8d850dae24165ca11c271b86eca58dfe8a8e
Osvaldo2308 <osvaldo245678@gmail.com>
2023-02-17 16:54:21 -0600

    last-Update

--- src/api/seacrhApi.ts
+++ src/api/seacrhApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchApi = axios.create({
     baseURL:'https://api.mapbox.com/geocoding/v5/mapbox.places',
     params:{
-        limit: 5,
+        limit: 4,
         Language: 'es',
         access_token:'pk.eyJ1Ijoib3N2YWxkbzI2diIsImEiOiJjbGU3ZXJiaTAwNHFsM3BvNm82dHd3d2h3In0.nZEaXfJlpltxi3l42SrycA'
     }
